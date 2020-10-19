6f885ce3e6dc343b8f814a06c719255a267c3932
Jonahhess <61464306+Jonahhess@users.noreply.github.com>
2020-10-19 18:52:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ void MakeSequence(long long arr[N],long long sequence[N]) {
             }
         }
         
-        sequence[0] += 1; /* comment out for the regular series */
+        sequence[0] += 1; /* comment out for the partition numbers */
         sequence[i] = sum;
     }
 }
@@ -63,7 +63,7 @@ int main() {
     return 0;
 }
 
-/* regular series (comment out line 41):
+/* partition numbers (comment out line 41):
 1 1 2 3 5 7 11 15 22 30 42 56 77 101 135 176 231 297 385 490 627 792 1002 
 1255 1575 1958 2436 3010 3718 4565 5604 6842 8349 10143 12310 14883 17977 
 21637 26015 31185 37338 44583 53174 63261 75175 89134 105558 124754 147273 
